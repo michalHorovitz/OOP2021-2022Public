c0265d66237a521c3e8012aa01a61ae2090597fa
michalHorovitz <michal.horovits@gmail.com>
2021-11-15 11:20:40 +0200

    update "getAngleInRange"

--- OOP-HW2-Complex/src/ComplexUtils.java
+++ OOP-HW2-Complex/src/ComplexUtils.java
@@ -1,13 +1,13 @@
 
 public class ComplexUtils {
-	
+
 	private static double PRECISION = 1.0e-5;
 
 	public static double getAngleInRange(double angle) {
 
 		if ((angle > Math.PI) || (angle <= (-1) * Math.PI)) {
-			int d = (int) (angle / Math.PI);
-			angle -= d * Math.PI;
+			int d = (int) (angle / (2 * Math.PI));
+			angle -= d * 2 * Math.PI;
 		}
 		return angle;
 	}
